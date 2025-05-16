a745ade6fb368307ffb620ad8282d2b10f4d3ea3
teo <teo.th@hotmail.gr>
2025-05-16 21:42:14 +0200

    added Cors configuration

---
+++ src/main/java/com/ai/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ai;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000") // 🚫 no trailing slash
+            .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
